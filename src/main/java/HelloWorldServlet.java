667178def4bc203e46bd93cdae68fbcad1333bb7
Jillian Wysoki <jillianwysoki@gmail.com>
2018-08-20 12:21:32 -0500

    refactoring of jsp exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -3,10 +3,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/")
 public class HelloWorldServlet extends HttpServlet {
+
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
         response.getWriter().println("<h1>Hello, World!</h1>");
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if(username == null && password == null){
+            username = "";
+        }
+        else if (username.equals("admin") && password.equals("password") ){
+            response.sendRedirect("/profile.jsp");
+        }
+
     }
+
+
 }
