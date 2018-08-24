5738fade3f1a8d49b399215f54f4b902fa535e91
Jillian Wysoki <jillianwysoki@gmail.com>
2018-08-24 18:43:35 -0500

    all

--- src/main/java/com/codeup/adlister/dao/DaoFactory.java
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -2,12 +2,14 @@ package com.codeup.adlister.dao;
 
 public class DaoFactory {
     private static Ads adsDao;
-    private static Config config = new Config();
 
     public static Ads getAdsDao() {
         if (adsDao == null) {
+            Config config = new Config();
             adsDao = new MySQLAdsDao(config);
         }
         return adsDao;
     }
+
+
 }
